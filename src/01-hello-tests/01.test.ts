e6004866aa9798484675ea263d5595081ed02533
NovikovRomanV <rimm19845@gmail.com>
2020-08-01 20:43:54 +0300

    del idea

---
+++ src/01-hello-tests/01.test.ts
@@ -0,0 +1,58 @@
+import {mult, splitIntoWords, sum} from "./01";
+
+//data
+let a: number;
+let b: number;
+let c: number;
+
+beforeEach(() => {
+   a = 1;
+   b = 2;
+   c = 3;
+});
+
+test('sum should be correct', () => {
+
+    //action
+    a = 100;
+    const result1 = sum(a, b);
+    const result2 = sum(b, c);
+
+    //expect result
+    expect(result1).toBe(102);
+    expect(result2).toBe(5);
+});
+
+test('multiply should be correct', () => {
+
+    //action
+    const result3 = mult(a, b);
+    const result4 = mult(b, c);
+
+    //expect
+    expect(result3).toBe(2);
+    expect(result4).toBe(6);
+});
+
+test('splitting into world should be correct', () => {
+    //data
+    let senten1 = 'Hello my friend!';
+    let senten2 = 'JS - the best  programming language.';
+
+    //action
+    const result1 = splitIntoWords(senten1);
+    const result2 = splitIntoWords(senten2);
+
+    //expect
+    expect(result1.length).toBe(3);
+    expect(result1[0]).toBe('hello');
+    expect(result1[1]).toBe('my');
+    expect(result1[2]).toBe('friend');
+
+    expect(result2.length).toBe(5);
+    expect(result2[0]).toBe('js');
+    expect(result2[1]).toBe('the');
+    expect(result2[2]).toBe('best');
+    expect(result2[3]).toBe('programming');
+    expect(result2[4]).toBe('language');
+});
